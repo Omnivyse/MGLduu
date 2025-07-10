ad6a28eb299531c7cab0b9e3086a1c1e31c5ed55
Your Name <bilguunz045@gmail.com>
2025-07-10 12:37:42 +0800

    update

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -4,7 +4,7 @@ const config = {
     API_BASE_URL: 'http://localhost:4000',
   },
   production: {
-    API_BASE_URL: process.env.REACT_APP_BACKEND_URL || 'https://mglduu-production.up.railway.app',
+    API_BASE_URL: process.env.REACT_APP_BACKEND_URL,
   }
 };
 
